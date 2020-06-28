731ca00c13dd2ac5219abc53bd89121f819672e0
jgaunt96 <66772604+jgaunt96@users.noreply.github.com>
2020-06-28 16:43:56 +0100

    databse

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ ob_start();
 try {
 
     // $con = new PDO("mysql:dbname=doodle;host=localhost", "root", "");
-    $con = new PDO("mysql:dbname=doodle:host=//eu-cdbr-west-03.cleardb.net/heroku_c20b2a94ec09272?reconnect=true", "b5d738cb55e2b2:95108ea6", "");
+    $con = new PDO("mysql:dbname=doodle;host=//eu-cdbr-west-03.cleardb.net/heroku_c20b2a94ec09272?reconnect=true", "b5d738cb55e2b2:95108ea6", "");
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 }
 catch(PDOException $e){
